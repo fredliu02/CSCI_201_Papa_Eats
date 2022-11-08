39c76479b1537ffe9479d040f91f7475d6748d6c
Frederick Liu <liufrede@usc.edu>
2022-11-07 17:54:56 -0800

    i

--- phlog/src/App.js
+++ phlog/src/App.js
@@ -1,14 +1,16 @@
 import * as React from "react";
-import logo from "./logo.svg";
+import { Routes, Link, Route, BrowserRouter } from "react-router-dom";
 import Button from "@mui/material/Button";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Router></Router>
+    <div>
+      <Routes>
+        <Route exact path="/a" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ phlog/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Navbar from "../../Components/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <h1>welcome!asdfsssdfasdfadsfadsfasdfasd</h1>
+    </div>
+  );
+}
+
+export default Home;
