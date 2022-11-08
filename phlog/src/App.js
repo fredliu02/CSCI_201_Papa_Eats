6a578ffcfc34b4d84506446a45a50c750c7823ef
Frederick Liu <liufrede@usc.edu>
2022-11-07 17:05:57 -0800

    i

--- phlog/src/App.js
+++ phlog/src/App.js
@@ -2,25 +2,14 @@ import * as React from "react";
 import logo from "./logo.svg";
 import Button from "@mui/material/Button";
 import "./App.css";
-import Navbar from "./Components/Navbar"
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome!</h1>
       </header>
     </div>
   );
--- phlog/src/Components/Navbar.js
+++ phlog/src/Components/Navbar.js
@@ -3,22 +3,32 @@ import Button from "@mui/material/Button";
 import AppBar from "@mui/material/AppBar";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import Box from "@mui/material/Box";
 
 function Navbar() {
   return (
-    <AppBar>
-      <IconButton
-        size="large"
-        edge="start"
-        color="inherit"
-        aria-label="menu"
-        sx={{ mr: 2 }}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          paddingLeft: 5,
+        }}
       >
-        <MenuIcon />
-      </IconButton>
-      <Button variant="contained">Contained</Button>
-      <h1>Phlog</h1>
-    </AppBar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Button sx={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
+          Phlog
+        </Button>
+      </AppBar>
+    </Box>
   );
 }
 
