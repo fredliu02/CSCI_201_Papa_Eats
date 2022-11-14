a257a2456e01d336e404c309f97ebb7f9b5b4af3
Frederick Liu <liufrede@usc.edu>
2022-11-14 11:11:59 -0800

    login

--- phlog/src/App.js
+++ phlog/src/App.js
@@ -1,15 +1,16 @@
 import * as React from "react";
 import { Routes, Link, Route, BrowserRouter } from "react-router-dom";
-import Button from "@mui/material/Button";
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import Home from "./pages/Home/Home";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route exact path="/a" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Login" element={<Login />} />
       </Routes>
     </div>
   );
