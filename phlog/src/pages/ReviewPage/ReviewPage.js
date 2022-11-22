9e82ba3de435d8af91e60fe8e36808659221e2b0
Amber Garcha <amberkaur1@gmail.com>
2022-11-21 16:56:16 -0800

    Added temp fetch and back button

--- phlog/src/pages/ReviewPage/ReviewPage.js
+++ phlog/src/pages/ReviewPage/ReviewPage.js
@@ -13,8 +13,35 @@ import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { useNavigate } from "react-router-dom";
 
-function Home() {
+function ReviewPage() {
+    const date = new Date();
+    const navigate = useNavigate();
+    const showTime = date.getHours() 
+        + ':' + date.getMinutes() 
+        + ":" + date.getSeconds();
+    var jsonData = {
+        "reviewID":100,
+        "userID": 0,
+        "restID":5,
+        "textRev":"Add review here",
+        "timePosted":showTime
+    }
+
+    function handleClick() {
+    
+    // Send data to the backend via POST
+    fetch('localhost:8081/api/restaurants/3/reviews', {  // Enter your IP address here
+
+      method: 'POST', 
+      mode: 'cors', 
+      body: JSON.stringify(jsonData) // body data type must match "Content-Type" header
+
+    });
+    navigate(-1); //navigate back to restaurant page
+  }
+    
   return (
     <div>
       <Box sx={{ flexGrow: 1, margin: "10%" }}>
@@ -33,14 +60,15 @@ function Home() {
             </FormControl>
           </Grid>
             <Grid item xs={12} style={{ textAlign: "center" }}>
-            <Button sx={{margin: "2%", bgcolor:"#49AF25", width:"25%", fontSize: "16px"}} variant="contained">
+            <Button sx={{margin: "2%", bgcolor:"#49AF25", width:"25%", fontSize: "16px"}} variant="contained"
+            onClick={handleClick}>
                         SUBMIT
             </Button>
         
           </Grid>
           <Grid item xs={12} style={{ textAlign: "center" , width: "10%"}}>
             <Button sx={{ bgcolor: "#C05757" }} variant="contained" 
-            onClick={event =>  window.location.href='/Restaurant'}>
+            onClick={() => navigate(-1)}>
                         CANCEL
             </Button>
         
@@ -53,4 +81,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default ReviewPage;
