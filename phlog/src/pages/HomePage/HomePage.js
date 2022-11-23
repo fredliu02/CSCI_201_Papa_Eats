8652c9c9920854b75eff2eb9ccbbebe4cf0e704a
Frederick Liu <liufrede@usc.edu>
2022-11-23 15:18:12 -0800

    done

--- phlog/src/pages/HomePage/HomePage.js
+++ phlog/src/pages/HomePage/HomePage.js
@@ -4,55 +4,51 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Card, CardContent, Grid } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
-import DeleteIcon from '@mui/icons-material/Delete';
-import { Routes, Route, useNavigate } from "react-router-dom";
-
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Routes, Route, useNavigate, useParams } from "react-router-dom";
 
 function HomePage() {
-
-
+  const { username } = useParams();
   const navigate = useNavigate();
-
   const navigateToAmerican = () => {
     // 👇️ navigate to /contacts
-    navigate('/American');
+    navigate("/American");
   };
 
   const navigateToItalian = () => {
     // 👇️ navigate to /contacts
-    navigate('/Italian');
+    navigate("/Italian");
   };
 
   const navigateToMexican = () => {
     // 👇️ navigate to /contacts
-    navigate('/Mexican');
+    navigate("/Mexican");
   };
 
   const navigateToChinese = () => {
     // 👇️ navigate to /contacts
-    navigate('/Chinese');
+    navigate("/Chinese");
   };
 
   const navigateToJapanese = () => {
     // 👇️ navigate to /contacts
-    navigate('/Japanese');
+    navigate("/Japanese");
   };
 
   const navigateToIndian = () => {
     // 👇️ navigate to /contacts
-    navigate('/Indian');
+    navigate("/Indian");
   };
 
   const navigateToCoffee = () => {
     // 👇️ navigate to /contacts
-    navigate('/Coffee');
+    navigate("/Coffee");
   };
 
   const navigateToOther = () => {
     // 👇️ navigate to /contacts
-    navigate('/Other');
+    navigate("/Other");
   };
-
 
   return (
     <div>
@@ -62,29 +58,24 @@ function HomePage() {
             <h1> --- search bar goes here ---</h1>
           </Grid>
           <Grid item xs={12} style={{ textAlign: "center" }}>
-            <h4>
-              Or... Pick a Cuisine!
-            </h4>
+            <h4>Or... Pick a Cuisine!</h4>
           </Grid>
         </Grid>
 
-
-
         <Grid container>
-
-
           {/* display 1st row of images */}
 
-
-
           <Grid item xs={3} style={{ textAlign: "center" }}>
-
-            <div style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center"
-            }}>
-              <img style={{ width: 200, height: 200 }} class="center"
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img
+                style={{ width: 200, height: 200 }}
+                class="center"
                 src="https://media.istockphoto.com/id/980513032/vector/hamburger-cheese-with-beef-salad-tomato-and-ham-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=J2RCdXakXuh0OmGNTYSI9CEDbJHGavnmijDWusO65qs="
                 alt="american"
               />
@@ -92,9 +83,10 @@ function HomePage() {
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-
             <div>
-              <img style={{ width: 170, height: 170 }} class="center"
+              <img
+                style={{ width: 170, height: 170 }}
+                class="center"
                 src="https://i.pinimg.com/originals/bd/83/75/bd8375b4bb1a6b22583acdd99be81ed0.png"
                 alt="italian"
               />
@@ -102,9 +94,10 @@ function HomePage() {
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-
             <div>
-              <img style={{ width: 170, height: 170 }} class="center"
+              <img
+                style={{ width: 170, height: 170 }}
+                class="center"
                 src="https://thumbs.dreamstime.com/b/taco-vector-illustration-72950754.jpg"
                 alt="mexican"
               />
@@ -112,64 +105,59 @@ function HomePage() {
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-
             <div>
-              <img style={{ width: 170, height: 170 }} class="center"
+              <img
+                style={{ width: 170, height: 170 }}
+                class="center"
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjFlympHnRbQ7rEzjOVLNXohBjFf4PTlFDiw&usqp=CAU"
                 alt="chinese"
               />
             </div>
           </Grid>
 
-
-
-
           {/* display 1st row of buttons */}
 
-
-
           <Grid item xs={3} style={{ textAlign: "center" }}>
-            <Button onClick = {navigateToAmerican} variant="contained">
+            <Button onClick={navigateToAmerican} variant="contained">
               American
             </Button>
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-            <Button onClick = {navigateToItalian} variant="contained">
+            <Button onClick={navigateToItalian} variant="contained">
               Italian
             </Button>
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-            <Button onClick = {navigateToMexican} variant="contained">
+            <Button onClick={navigateToMexican} variant="contained">
               Mexican
             </Button>
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-            <Button onClick = {navigateToChinese} variant="contained">
+            <Button onClick={navigateToChinese} variant="contained">
               Chinese
             </Button>
           </Grid>
-
         </Grid>
 
         <br />
 
         <Grid container>
-
-
           {/* display 2nd row of images */}
 
-
           <Grid item xs={3} style={{ textAlign: "center" }}>
-
-            <div style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center"
-            }}>
-              <img style={{ width: 200, height: 200 }} class="center"
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img
+                style={{ width: 200, height: 200 }}
+                class="center"
                 src="https://img.freepik.com/premium-vector/sushi-vector-clipart-as-lunch_616819-386.jpg?w=2000"
                 alt="japanese"
               />
@@ -177,9 +165,10 @@ function HomePage() {
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-
             <div>
-              <img style={{ width: 170, height: 170 }} class="center"
+              <img
+                style={{ width: 170, height: 170 }}
+                class="center"
                 src="https://static.vecteezy.com/system/resources/thumbnails/002/252/420/small/hot-butter-chicken-cartoon-illustration-traditional-indian-food-meat-in-spiced-tomato-sauce-flat-color-object-restaurant-meal-served-makhani-chicken-isolated-on-white-background-vector.jpg"
                 alt="indian"
               />
@@ -187,9 +176,10 @@ function HomePage() {
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-
             <div>
-              <img style={{ width: 170, height: 170 }} class="center"
+              <img
+                style={{ width: 170, height: 170 }}
+                class="center"
                 src="https://illustoon.com/photo/thum/3814.png"
                 alt="coffee"
               />
@@ -197,51 +187,45 @@ function HomePage() {
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-
             <div>
-              <img style={{ width: 170, height: 170 }} class="center"
+              <img
+                style={{ width: 170, height: 170 }}
+                class="center"
                 src="https://i.pinimg.com/originals/65/57/3b/65573bbec6ec8cc68ba0a6541633fcd5.jpg"
                 alt="other"
               />
             </div>
-
-
           </Grid>
 
           {/* display 2nd row of buttons */}
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-            <Button onClick = {navigateToJapanese} variant="contained">
+            <Button onClick={navigateToJapanese} variant="contained">
               Japanese
             </Button>
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-            <Button onClick = {navigateToIndian} variant="contained">
+            <Button onClick={navigateToIndian} variant="contained">
               Indian
             </Button>
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-            <Button onClick = {navigateToCoffee} variant="contained">
+            <Button onClick={navigateToCoffee} variant="contained">
               Coffee
             </Button>
           </Grid>
 
           <Grid item xs={3} style={{ textAlign: "center" }}>
-            <Button onClick = {navigateToOther} variant="contained">
+            <Button onClick={navigateToOther} variant="contained">
               Other
             </Button>
           </Grid>
         </Grid>
-
-
       </Box>
     </div>
   );
 }
-
-
-
 
 export default HomePage;
--- phlog/src/pages/RestaurantPage/RestaurantPage.js
+++ phlog/src/pages/RestaurantPage/RestaurantPage.js
@@ -1,18 +1,149 @@
 import * as React from "react";
-
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import RestaurantReview from "../../Components/RestaurantReview";
+import { Button } from "@mui/material";
+import Navbar from "../../Components/Navbar/Navbar";
+import AuthNavbar from "../../Components/AuthNavbar/AuthNavbar";
 
 function RestaurantPage() {
-  const numbers = [1, 2, 3, 4, 5];
-  const listItems = numbers.map((numbers) => (
-    // <li>
-    <RestaurantReview name="Frederick Liu" date="sept 5, 2022" review="yum" />
-    // </li>
-  ));
+  const { username, restaurant } = useParams();
+  const [reviews, setReviews] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    getReviews(username, restaurant);
+  });
+
+  const getReviews = async (username, restaurant) => {
+    let vef = await verify(username);
+    if (!vef) {
+      navigate("/Error");
+      return;
+    }
+    const response = await fetch(
+      `https://future-badge-366719.uw.r.appspot.com/api/restaurants/name/${restaurant}`,
+      {
+        Method: "GET",
+        Headers: {
+          Accept: "application.json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    let data = null;
+    try {
+      data = await response.json();
+    } catch (err) {
+      console.log(err);
+      navigate("/Error");
+      return;
+    }
+
+    const reviews = await fetch(
+      `https://future-badge-366719.uw.r.appspot.com/api/restaurants/${data.id}/reviews`,
+      {
+        Method: "GET",
+        Headers: {
+          Accept: "application.json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    try {
+      data = await reviews.json();
+    } catch (err) {
+      console.log(err);
+      alert("failed to retrieve reivews!");
+      return;
+    }
+    setReviews(data);
+  };
+  const actions = (username) => {
+    if (username === ":GUEST:") {
+      return (
+        <div>
+          <h3>Sign-up to write a review</h3>
+          <Button
+            variant="contained"
+            onClick={() => {
+              navigate(`/Signup`);
+            }}
+          >
+            Signup
+          </Button>
+        </div>
+      );
+    } else {
+      return (
+        <Button
+          variant="contained"
+          onClick={() => {
+            navigate(`/${username}/Homepage`);
+          }}
+        >
+          Write a review
+        </Button>
+      );
+    }
+  };
+  const verify = async (username) => {
+    if (username === ":GUEST:") {
+      return true;
+    }
+    const response = await fetch(
+      "https://future-badge-366719.uw.r.appspot.com/api/users/username/" +
+        username,
+      {
+        Method: "GET",
+        Headers: {
+          Accept: "application.json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    let data = null;
+    try {
+      data = await response.json();
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  };
+  const navbar = (username) => {
+    if (username === ":GUEST:") {
+      return <Navbar />;
+    } else {
+      return <AuthNavbar />;
+    }
+  };
+
   return (
-    <div style={{ margin: "5%" }}>
-      <h1>Restaurant</h1>
-      <ul>{listItems}</ul>
+    <div>
+      <AuthNavbar />
+      <div
+        style={{
+          marginTop: "5%",
+          width: "100%",
+        }}
+      >
+        <h1 style={{ width: "100%", textAlign: "center" }}>{restaurant}</h1>
+        <div style={{ width: "100%", textAlign: "center" }}>
+          {actions(username)}
+        </div>
+
+        <ul>
+          {reviews.map((review) => (
+            <RestaurantReview
+              name={String(review.username)}
+              date={String(review.timePosted)}
+              review={String(review.textRev)}
+            />
+          ))}
+        </ul>
+        <h2 style={{ width: "100%", textAlign: "center" }}>End of Reviews</h2>
+      </div>
     </div>
   );
 }
