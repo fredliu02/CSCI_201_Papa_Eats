5656dad5ffaed19a00f1807e963cbcf5ac69d882
Frederick Liu <liufrede@usc.edu>
2022-11-29 20:23:21 -0800

    descript

--- phlog/src/Pages/Home/Home.js
+++ phlog/src/Pages/Home/Home.js
@@ -53,18 +53,11 @@ function Home() {
             <h1>Welcome to Phlog!</h1>
           </Grid>
           <Grid item xs={12} style={{ textAlign: "center" }}>
-            <h4>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </h4>
+            <h3>
+              Phlog strives to bring people together through food, connect with
+              your friends to discover restaurants in your area and see the
+              restaurants they love.
+            </h3>
           </Grid>
           <Grid item xs={12}>
             <div>
