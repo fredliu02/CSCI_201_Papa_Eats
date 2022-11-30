fb3e0394af8697d3efb0008e8d08c296e4f42702
Frederick Liu <liufrede@usc.edu>
2022-11-29 19:53:50 -0800

    login

---
+++ phlog/src/Pages/MexicanPage/MexicanPage.js
@@ -0,0 +1,100 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Paper from '@mui/material/Paper';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+import { Grid } from "@mui/material";
+import { useState } from "react";
+import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+import AuthNavbar from "../../Components/AuthNavbar/AuthNavbar";
+
+function MexicanPage() {
+    const { username } = useParams();
+    const [data, setData] = useState([]);
+    const navigate = useNavigate();
+
+    const apiGet = () => {
+        fetch("https://future-badge-366719.uw.r.appspot.com/api/restaurants/cuisine/Mexican")
+            // fetch("https://jsonplaceholder.typicode.com/posts")
+
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+                setData(json);
+            })
+    }
+
+    return (
+        <div>
+        <AuthNavbar username={username}/>
+        <Box sx={{ flexGrow: 1, marginTop: "2%"}}>
+            <Grid container>
+                <Grid item xs={12} style={{ textAlign: "center" }}>
+                    <h1> Welcome to the Mexican Cuisine Page! </h1>
+                    <h4> Click to display restaurants below: </h4>
+                    <Button onClick={apiGet} variant="contained">
+                        Load Mexican Restaurants
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>        
+                        <ul>
+                            {data.map((item) => (  
+                                <Paper 
+                                    elevation={1} 
+                                    sx={{
+                                        marginLeft: "25%",
+                                        marginRight: "25%",
+                                        marginTop: "1%",
+                                        borderRadius: "25px",
+                                        height: "60px",
+                                        backgroundColor: "#8cdcff",
+                                    }}
+                                >          
+                                    <br/>                            
+                                    <div style={{display: "inline-block", marginLeft: "20px"}}>
+                                        {item.name}
+                                        <Button startIcon={<ArrowForwardIcon/>} 
+                                            onClick={() => {navigate("/" + username + "/R/" + item.name)}}
+                                        >
+                                        </Button>
+                                    </div>
+                                    <br/>
+                                </Paper>
+        
+                                // <li key={item.id}>{item.name}</li>
+                            ))}
+                             <br/>
+                         </ul>
+                </Grid>
+            </Grid>
+        </Box>
+    </div>
+    );
+
+    // {/* return (
+
+
+    //         <div>
+    //             <h1> American Cuisine</h1>
+    //             <br />
+
+    //             {/* <button onClick={apiGet}> Load American Cuisines</button> */}
+
+    // <br />
+
+    //         </div >
+
+
+
+
+    // ); * /}
+}
+
+
+
+
+export default MexicanPage;
